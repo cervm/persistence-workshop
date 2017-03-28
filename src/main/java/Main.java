f5e98ad4a1ce1ea49079822a6b382026feffec3d
cervm <marek.cervinka@gmail.com>
2017-03-28 15:42:24 +0200

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,16 @@
-/**
- * Created by marekcervinka on 28/03/2017.
- */
-
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * Western Style App
+ *
+ * @author Marek Cervinka
+ * @author Andrei Katona
+ * @author Jakub Sliva
+ * @author Ondrej Soukup
+ * @author Rajmund Staniek
+ * @version 1.0
+ */
 public class Main extends Application {
 
     public static void main(String[] args) {
