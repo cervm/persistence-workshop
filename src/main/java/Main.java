3a017a6fef071d69a709088842d5342080d5d1a6
cervm <marek.cervinka@gmail.com>
2017-03-28 15:11:47 +0200

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Created by marekcervinka on 28/03/2017.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
