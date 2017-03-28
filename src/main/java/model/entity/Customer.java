08c907a332c3d6346b2af801ccaaf714ac5a3e78
Rajmund Staniek <rajmund.st@gmail.com>
2017-03-28 22:17:10 +0200

    fixed design pattern

--- src/main/java/model/entity/Customer.java
+++ src/main/java/model/entity/Customer.java
@@ -1,14 +1,11 @@
 package model.entity;
 
 import model.CustomerGroup;
-import model.IDataAccessObject;
-
-import java.util.List;
 
 /**
  * Created by rajmu on 17.03.28.
  */
-public class Customer extends ContactDetails implements IDataAccessObject<Customer> {
+public class Customer extends ContactDetails {
     private int cid;
     private CustomerGroup group; //TODO: needs to be somehow initialized
     private String name;
@@ -64,30 +61,5 @@ public class Customer extends ContactDetails implements IDataAccessObject<Custom
     @Override
     protected String getCountry() {
         return super.getCountry();
-    }
-
-    @Override
-    public List<Customer> getAll() {
-        return null; //TODO: to be implemented
-    }
-
-    @Override
-    public Customer getById(int id) {
-        return null; //TODO: to be implemented
-    }
-
-    @Override
-    public void create(Customer object) {
-        //TODO: to be implemented
-    }
-
-    @Override
-    public void update(Customer object) {
-        //TODO: to be implemented
-    }
-
-    @Override
-    public void delete(Customer object) {
-        //TODO: to be implemented
     }
 }
